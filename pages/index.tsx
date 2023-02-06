afdd18fb768c96b03e85b7b69eb73e04e06610ad
balogunvictor <praisetheultimate@gmail.com>
2023-02-06 22:39:48 +0100

    mapped Movies into app

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home = ({
   // products,
 }: Props) => { 
   return (
-    <div className='relative h-screen bg-gradient-to-b from-gray-900/10
+    <div className='relative h-screen bg-gradient-to-b
     to-[#010511] lg:h-[140vh ]'>
       <Head>
         <title>Home - Netflix</title>
